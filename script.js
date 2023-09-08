ab5ac7f8068a81927ea70f8526b6167ffb7a2c58
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-09-08 20:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+const form = document.getElementById('form');
+const url = document.getElementById('url');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = form['name'].value;
+    const year = form['year'].value;
+
+	let getURL = `${url.innerText}?`;
+
+    if(name){
+		getURL += `name=${encodeURIComponent(name)}&`;
+	}
+
+	if(year){
+		getURL += `year=${encodeURIComponent(year)}`;
+	}
+	
+    url.innerHTML = getURL;
+});
